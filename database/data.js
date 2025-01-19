fd3bc7a37082fc32db54029173de6c9f85419736
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2025-01-19 20:34:18 +0530

    Adding new questions

--- database/data.js
+++ database/data.js
@@ -1,84 +1,28 @@
-// export default [
-//     {
-//         id: 1,
-//         question : "Javascript is an _______ language",
-//         options : [
-//             'Object-Oriented',
-//             'Object-Based',
-//             'Procedural',
-//         ]
-//     },
-//     {
-//         id: 2,
-//         question : "Following methods can be used to display data in some form using Javascript",
-//         options : [
-//             'document.write()',
-//             'console.log()',
-//             'window.alert()',
-//         ]
-//     },
-//     {
-//         id: 3,
-//         question : "When an operator value is NULL, the typeof returned by the unary operator is:",
-//         options : [
-//             'Boolean',
-//             'Undefined',
-//             'Object',
-//         ]
-//     },
-//     {
-//         id: 4,
-//         question : "What does the toString() method return?",
-//         options : [
-//             'Return Object',
-//             'Return String',
-//             'Return Integer'
-//         ]
-//     },
-//     {
-//         id: 5,
-//         question : "Which function is used to serialize an object into a JSON string?",
-//         options : [
-//             'stringify()',
-//             'parse()',
-//             'convert()',
-//         ]
-//     }
-// ];
-
-// export const answers = [0, 1, 2, 1, 0];
-
-
 export default [
-    {'id': 1,
-    'question': 'Supplier of Amifix Blue R Special',
-    'options': ['DyStar', 'COLORBAND', 'Atul']},
-   {'id': 2,
-    'question': 'Supplier of Amifix Turq. Blue G',
-    'options': ['Atul', 'KISCO', 'CHT India Pvt Ltd']},
-   {'id': 3,
-    'question': 'Supplier of Amron HF Amr. Red HF3GL',
-    'options': ['KISCO', 'Amtex Dye Chem', 'Jay Dyes & Chem']},
-   {'id': 4,
-    'question': 'Supplier of Amron HF Amr. Navy HFBF',
-    'options': ['COLORBAND', 'Ciba', 'DyStar']},
-   {'id': 5,
-    'question': 'Supplier of Amron HF Amr. Yellow HF3R',
-    'options': ['Local supplier', 'KISCO', 'CHT India Pvt Ltd']},
-   {'id': 6,
-    'question': 'Supplier of Avitera SE Avitera Navy SE',
-    'options': ['COLORBAND', 'CHT India Pvt Ltd', 'KISCO']},
-   {'id': 7,
-    'question': 'Supplier of Bzaktive Black SNN 01',
-    'options': ['Ardip Agency', 'COLORBAND', 'KISCO']},
-   {'id': 8,
-    'question': 'Supplier of Cbcrylic Cbcryl Blue GRL 300%',
-    'options': ['Local supplier', 'KISCO', 'Ardip Agency']},
-   {'id': 9,
-    'question': 'Supplier of Everacid Everacid Black X-BW',
-    'options': ['Local supplier', 'KISCO', 'Ardip Agency']},
-   {'id': 10,
-    'question': 'Supplier of Jakazol CE - DIFF 60 Jak. Navy CE',
-    'options': ['Local supplier', 'CHT India Pvt Ltd', 'Atul']}
-]
-    export const answers = [0, 1, 0, 2, 0, 0, 1, 0, 0, 0]
+    { 'id': 1, 'question': 'Supplier of Amifix Blue R Special', 'options': ['Amtex Dye Chem', 'COLORBAND', 'DyStar'] },
+    { 'id': 2, 'question': 'Supplier of Amifix Turq. Blue G', 'options': ['Amtex Dye Chem', 'KISCO', 'CHT India Pvt Ltd'] },
+    { 'id': 3, 'question': 'Supplier of Amron HF Amr. Red HF3GL', 'options': ['Amtex Dye Chem', 'KISCO', 'Jay Dyes & Chem'] },
+    { 'id': 4, 'question': 'Supplier of Avitera SE Avitera Navy SE', 'options': ['Ciba', 'COLORBAND', 'KISCO'] },
+    { 'id': 5, 'question': 'Supplier of Levafix Brill Blue E-FFN', 'options': ['DyStar', 'COLORBAND', 'CHT India Pvt Ltd'] },
+    { 'id': 6, 'question': 'Supplier of Syn Ultra Navy DS', 'options': ['KISCO', 'DyStar', 'Amtex Dye Chem'] },
+    { 'id': 7, 'question': 'Supplier of Everacid Everacid Black X-BW', 'options': ['COLORBAND', 'Ardip Agency', 'Local supplier'] },
+    { 'id': 8, 'question': 'Supplier of Jakazol CE - DIFF 60 Jak. Navy CE', 'options': ['Jay Dyes & Chem', 'Atul', 'Local supplier'] },
+    { 'id': 9, 'question': 'Supplier of Coralene Navy Blue HP-GS', 'options': ['Ardip Agency', 'COLORBAND', 'KISCO'] },
+    { 'id': 10, 'question': 'Supplier of Remazol Turquoise Blue G', 'options': ['DyStar', 'Amtex Dye Chem', 'COLORBAND'] },
+    { 'id': 11, 'question': 'Supplier of Procion HE Red HE7B', 'options': ['Atul', 'DyStar', 'Ciba'] },
+    { 'id': 12, 'question': 'Supplier of Dianix Dianix Black XF2', 'options': ['DyStar', 'COLORBAND', 'Ciba'] },
+    { 'id': 13, 'question': 'Supplier of Procion Navy Blue HER', 'options': ['Atul', 'DyStar', 'Ciba'] },
+    { 'id': 14, 'question': 'Supplier of Synozol Navy EC', 'options': ['KISCO', 'COLORBAND', 'DyStar'] },
+    { 'id': 15, 'question': 'Supplier of Terasil Blue 3RL-02', 'options': ['Ciba', 'DyStar', 'Amtex Dye Chem'] },
+    { 'id': 16, 'question': 'Supplier of Coralene Brill Red HP-3B', 'options': ['Ardip Agency', 'Ciba', 'CHT India Pvt Ltd'] },
+    { 'id': 17, 'question': 'Supplier of Terasil Turq W-XN', 'options': ['Ciba', 'COLORBAND', 'Ardip Agency'] },
+    { 'id': 18, 'question': 'Supplier of Cbcrylic Red GRL 200%', 'options': ['COLORBAND', 'CHT India Pvt Ltd', 'KISCO'] },
+    { 'id': 19, 'question': 'Supplier of Amron HF Amr. Navy HFBF', 'options': ['DyStar', 'Ciba', 'COLORBAND'] },
+    { 'id': 20, 'question': 'Supplier of Bzaktive Black SNN 01', 'options': ['CHT India Pvt Ltd', 'KISCO', 'Ardip Agency'] },
+    { 'id': 21, 'question': 'Supplier of Remazol Blue RGB', 'options': ['DyStar', 'Atul', 'KISCO'] },
+    { 'id': 22, 'question': 'Supplier of Avitera Yellow SE', 'options': ['Ciba', 'COLORBAND', 'KISCO'] },
+    { 'id': 23, 'question': 'Supplier of Everacid Everacid Red ARL', 'options': ['COLORBAND', 'Ardip Agency', 'CHT India Pvt Ltd'] },
+    { 'id': 24, 'question': 'Supplier of Procian Blue HEXL', 'options': ['Atul', 'DyStar', 'COLORBAND'] },
+    { 'id': 25, 'question': 'Supplier of Amifix Turq. Blue G', 'options': ['Amtex Dye Chem', 'KISCO', 'CHT India Pvt Ltd'] }
+];
+export const answers = [0, 2, 0, 2, 0, 1, 0, 2, 1, 0, 1, 2, 0, 2, 1, 1, 2, 1, 2, 0, 1, 2, 1, 2, 1];
