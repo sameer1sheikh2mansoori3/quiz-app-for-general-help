6b31d9b6da6f9f51d0acd7ebea844aeb28d0b883
sameer1sheikh2mansoori3 <94507751+sameer1sheikh2mansoori3@users.noreply.github.com>
2025-01-19 19:05:30 +0530

    first commit

---
+++ database/data.js
@@ -0,0 +1,84 @@
+// export default [
+//     {
+//         id: 1,
+//         question : "Javascript is an _______ language",
+//         options : [
+//             'Object-Oriented',
+//             'Object-Based',
+//             'Procedural',
+//         ]
+//     },
+//     {
+//         id: 2,
+//         question : "Following methods can be used to display data in some form using Javascript",
+//         options : [
+//             'document.write()',
+//             'console.log()',
+//             'window.alert()',
+//         ]
+//     },
+//     {
+//         id: 3,
+//         question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+//         options : [
+//             'Boolean',
+//             'Undefined',
+//             'Object',
+//         ]
+//     },
+//     {
+//         id: 4,
+//         question : "What does the toString() method return?",
+//         options : [
+//             'Return Object',
+//             'Return String',
+//             'Return Integer'
+//         ]
+//     },
+//     {
+//         id: 5,
+//         question : "Which function is used to serialize an object into a JSON string?",
+//         options : [
+//             'stringify()',
+//             'parse()',
+//             'convert()',
+//         ]
+//     }
+// ];
+
+// export const answers = [0, 1, 2, 1, 0];
+
+
+export default [
+    {'id': 1,
+    'question': 'Supplier of Amifix Blue R Special',
+    'options': ['DyStar', 'COLORBAND', 'Atul']},
+   {'id': 2,
+    'question': 'Supplier of Amifix Turq. Blue G',
+    'options': ['Atul', 'KISCO', 'CHT India Pvt Ltd']},
+   {'id': 3,
+    'question': 'Supplier of Amron HF Amr. Red HF3GL',
+    'options': ['KISCO', 'Amtex Dye Chem', 'Jay Dyes & Chem']},
+   {'id': 4,
+    'question': 'Supplier of Amron HF Amr. Navy HFBF',
+    'options': ['COLORBAND', 'Ciba', 'DyStar']},
+   {'id': 5,
+    'question': 'Supplier of Amron HF Amr. Yellow HF3R',
+    'options': ['Local supplier', 'KISCO', 'CHT India Pvt Ltd']},
+   {'id': 6,
+    'question': 'Supplier of Avitera SE Avitera Navy SE',
+    'options': ['COLORBAND', 'CHT India Pvt Ltd', 'KISCO']},
+   {'id': 7,
+    'question': 'Supplier of Bzaktive Black SNN 01',
+    'options': ['Ardip Agency', 'COLORBAND', 'KISCO']},
+   {'id': 8,
+    'question': 'Supplier of Cbcrylic Cbcryl Blue GRL 300%',
+    'options': ['Local supplier', 'KISCO', 'Ardip Agency']},
+   {'id': 9,
+    'question': 'Supplier of Everacid Everacid Black X-BW',
+    'options': ['Local supplier', 'KISCO', 'Ardip Agency']},
+   {'id': 10,
+    'question': 'Supplier of Jakazol CE - DIFF 60 Jak. Navy CE',
+    'options': ['Local supplier', 'CHT India Pvt Ltd', 'Atul']}
+]
+    export const answers = [0, 1, 0, 2, 0, 0, 1, 0, 0, 0]
